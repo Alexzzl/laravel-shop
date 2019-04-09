1c843bea4f878d95a740041229be744f5f9849ef
Alex961120 <805119233@qq.com>
2019-04-09 16:07:06 +0000

    订单支付

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,7 +18,9 @@ class AppServiceProvider extends ServiceProvider
         // 往服务容器中注入一个名为 alipay 的单例对象
         $this->app->singleton('alipay', function (){
             $config = config('pay.alipay');
-            // 判斷当前项目运行环境是否为线上环球
+            $config['notify_url'] = 'https://requestbin.leo108.com/1iifnjk1';
+            $config['return_url'] = route('payment.alipay.return');
+            // 判斷当前项目运行环境是否为线上环境
             if (app()->environment() !== 'production') {
                 $config['mode'] = 'dev';
                 $config['log']['level'] = Logger::DEBUG;
